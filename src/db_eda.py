68e3d2ab375294794582627d2ce1784bfd731559
10197jsg <10197jsg@gmail.com>
2025-03-22 16:17:57 +0100

    updates on readme, folders, and org

---
+++ src/db_eda.py
@@ -0,0 +1,8 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+#def print_lineplot(movies_w_ratings):
+
+    # Lineplot for numerical variables
+    #sns.lineplot(x = "imdb_rating", y = "spoiled_percentage", data = movies_w_ratings)
+    #plt.show()
